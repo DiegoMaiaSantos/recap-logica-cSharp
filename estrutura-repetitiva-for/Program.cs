ce153dc5fbb223c918d2b3931e1884976bf52e21
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-19 12:09:39 -0300

    Estrutura repetitiva for - Revisão.

---
+++ estrutura-repetitiva-for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantas vezes quer repetir? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int cont = 1; cont <= n; cont++)
+            {
+                Console.Write("Valor #" + cont + ": ");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
